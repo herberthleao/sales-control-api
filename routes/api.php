9c6e25dfc0c24937b19c16a7d1de6e15fbe8907d
Herberth Leão <git@herberthleao.me>
2023-09-05 21:15:22 -0300

    feat: add API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DivisionController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\SellerController;
+use App\Http\Controllers\SaleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/tokens', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get(
+        '/divisions',
+        [DivisionController::class, 'retrieveAll']
+    )->middleware('ability:read-divisions');
+
+    Route::get(
+        '/divisions/{id}/units',
+        [UnitController::class, 'retrieveAll']
+    )->whereNumber('id')->middleware('ability:read-units');
+
+    Route::get(
+        '/divisions/{divisionID}/units/{unitID}/sellers',
+        [SellerController::class, 'retrieveAll']
+    )->whereNumber([
+        'divisionID',
+        'unitID'
+    ])->middleware('ability:read-sellers');
+
+    Route::post(
+        '/sales',
+        [SaleController::class, 'register']
+    )->middleware('ability:create-sales');
+    Route::get(
+        '/sales',
+        [SaleController::class, 'retrieveAll']
+    )->middleware('ability:read-sales');
+    Route::get(
+        '/sales/{id}',
+        [SaleController::class, 'retrieve']
+    )->whereNumber('id')->middleware('ability:read-sales');
 });
