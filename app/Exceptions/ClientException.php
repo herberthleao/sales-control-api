0267047d039b670e385252fff62d174612ab73e9
Herberth Leão <git@herberthleao.me>
2023-09-05 21:08:05 -0300

    feat: add an exception type for api client errors

---
+++ app/Exceptions/ClientException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Exceção lançada quando o cliente não interage adequadamente com a API.
+ */
+class ClientException extends RuntimeException
+{}
